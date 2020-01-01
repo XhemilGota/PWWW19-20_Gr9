c87a72e7aefe40f3b3cfa108297a62be8e4725ad
ZgjimHaziri <zgjim.haziri@studentet.uni-pr.edu>
2020-01-01 23:32:13 +0100

    Added photos on 'Homepage' and created canvas on the 'Buy' page.

---
+++ js/Canvas.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("emoji");
+var ctx = canvas.getContext("2d");
+
+var radius = 100;
+var x_pos = 100;
+var y_pos = 150;
+
+var grd = ctx.createRadialGradient(x_pos,y_pos,radius,x_pos,y_pos,radius - 2/20*radius)
+grd.addColorStop(1, "#FECC49");
+grd.addColorStop(0, "#EE2C2C");
+
+circle(x_pos, y_pos, radius,grd);
+
+ctx.beginPath();
+ctx.moveTo(x_pos - 1/2*radius,y_pos+3/5*radius);
+ctx.quadraticCurveTo(x_pos,y_pos,x_pos+1/2*radius,y_pos+3/5*radius);
+ctx.stroke();
+
+circle(x_pos - 1/3*radius,y_pos-1.7/5*radius, radius/7,"#5C342C");
+circle(x_pos + 1/3*radius,y_pos-1.7/5*radius, radius/7, "#5C342C");
+
+ctx.font = "30px Arial";
+ctx.fillText("Oops...", x_pos + radius + 20, y_pos - radius + radius/2);
+ctx.font = "25px Arial";
+ctx.fillText("we can't find what you", x_pos + radius + 20, y_pos - radius + radius/2 + 35);
+ctx.fillText("are looking for!", x_pos + radius + 20, y_pos - radius + radius/2 + 60);
+
+
+
+function circle(x,y,r,color)
+{
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.stroke();
+    ctx.fill();
+}
+
--- js/homepage.js
+++ js/homepage.js
@@ -3,37 +3,100 @@ function expand(img)
 	document.getElementById("expandedImg").src = img.src;
 }
 
-var photos = [["img/homepage/950Lombard-big-1.jpg", 
-				"img/homepage/950Lombard-big-2.jpg", 
-				"img/homepage/950Lombard-big-3.jpg", 
-				"img/homepage/950Lombard-big-4.jpg", 
-				"img/homepage/950Lombard-big-5.jpg", 
-				"img/homepage/950Lombard-big-6.jpg", 
-				"img/homepage/950Lombard-big-7.jpg", 
-				"img/homepage/950Lombard-big-8.jpg"],
-				["img/homepage/950Lombard-big-1.jpg", 
-				"img/homepage/950Lombard-big-2.jpg", 
-				"img/homepage/950Lombard-big-3.jpg", 
-				"img/homepage/950Lombard-big-4.jpg", 
-				"img/homepage/950Lombard-big-5.jpg", 
-				"img/homepage/950Lombard-big-6.jpg", 
-				"img/homepage/950Lombard-big-7.jpg", 
-				"img/homepage/950Lombard-big-8.jpg"]];
+var photos = [["img/homepage/1/main.jpg", 
+				"img/homepage/1/other1.jpg", 
+				"img/homepage/1/other2.jpg", 
+				"img/homepage/1/other3.jpg", 
+				"img/homepage/1/other4.jpg", 
+				"img/homepage/1/other5.jpg", 
+				"img/homepage/1/other6.jpg"],
 
-var street = ["950 Lombard St", "950 Lombard St"];
+				["img/homepage/2/950Lombard-big-1.jpg", 
+				"img/homepage/2/950Lombard-big-2.jpg", 
+				"img/homepage/2/950Lombard-big-3.jpg", 
+				"img/homepage/2/950Lombard-big-4.jpg", 
+				"img/homepage/2/950Lombard-big-5.jpg", 
+				"img/homepage/2/950Lombard-big-6.jpg", 
+				"img/homepage/2/950Lombard-big-7.jpg", 
+				"img/homepage/2/950Lombard-big-8.jpg"],
+                            
+                                ["img/homepage/3/main.jpg", 
+				"img/homepage/3/other1.jpg", 
+				"img/homepage/3/other2.jpg", 
+				"img/homepage/3/other3.jpg", 
+				"img/homepage/3/other4.jpg", 
+				"img/homepage/3/other5.jpg", 
+				"img/homepage/3/other6.jpg"],
+                            
+                                ["img/homepage/4/main.jpg", 
+				"img/homepage/4/other1.jpg", 
+				"img/homepage/4/other2.jpg", 
+				"img/homepage/4/other3.jpg", 
+				"img/homepage/4/other4.jpg"],
+                            
+                                ["img/homepage/5/main.jpg",
+				"img/homepage/5/other1.jpg",
+				"img/homepage/5/other2.jpg",
+				"img/homepage/5/other3.jpg",
+				"img/homepage/5/other4.jpg",
+                                "img/homepage/5/other5.jpg",
+                                "img/homepage/5/other6.jpg"],
 
-var city = ["San Francisco, CA 94133, Russian Hill", "San Francisco, CA 94133, Russian Hill"];
+				["img/homepage/6/main.jpg",
+					"img/homepage/6/other1.jpg",
+					"img/homepage/6/other2.jpg",
+					"img/homepage/6/other3.jpg",
+					"img/homepage/6/other4.jpg",
+					"img/homepage/6/other5.jpg",
+					"img/homepage/6/other6.jpg",
+					"img/homepage/6/other7.jpg",
+					"img/homepage/6/other8.jpg"],
 
-var bedroom = [6, 6];
+				["img/homepage/7/main.jpg",
+					"img/homepage/7/other1.jpg",
+					"img/homepage/7/other2.jpg",
+					"img/homepage/7/other3.jpg",
+					"img/homepage/7/other4.jpg",
+					"img/homepage/7/other5.jpg"],
 
-var bathroom = [3, 3];
+					["img/homepage/8/main.jpg",
+					"img/homepage/8/other1.jpg",
+					"img/homepage/8/other2.jpg",
+					"img/homepage/8/other3.jpg",
+					"img/homepage/8/other4.jpg",
+					"img/homepage/8/other5.jpg",
+					"img/homepage/8/other6.jpg"],
 
-var sqrt = [5000, 5000];
+				["img/homepage/9/main.jpg",
+				"img/homepage/9/other1.jpg",
+				"img/homepage/9/other2.jpg",
+				"img/homepage/9/other3.jpg",
+				"img/homepage/9/other4.jpg",
+				"img/homepage/9/other5.jpg"]
+                        ];
 
-var price = ["$40,500,000", "$40,500,000"];
+var street = ["2646 Union St", "950 Lombard St", "11526 Wendover Ln", "3102 Ferndale St", "2448 E 12th St","1218 Roberto Lane","10250 W SUNSET","1160 SAN YSIDRO Drive", "2223 QUEENSBOROUGH Lane"];
 
-var descriptions = ["RESIDENCE 950...San Francisco's Newest Masterpiece. Nestled atop a magical park-like setting on one of the City's largest residential lots, this world-class urban estate has been brilliantly re-imagined for the future. A dramatic 40 foot cantilevered infinity pool reflects the shimmering Bay and glittering lights of the City's famed Skyline, while dazzling views of iconic landmarks from Salesforce Tower to The Transamerica Pyramid and beyond provide a stunning panorama surrounding the fabulous resort-like indoor/ outdoor flow of living, dining and entertainment spaces- easily accommodating 300+ guests. The super-advanced, technology enabled health and wellness environment of this sustainably built LEED Platinum Certified, exquisitely designed property, as well as the spa-like guest cottage with steam, sauna, massage room, outdoor shower and hot tub, await the most discerning of owners. Experience the magic of one of California's most extraordinary private residences...",
-					"RESIDENCE 950...San Francisco's Newest Masterpiece. Nestled atop a magical park-like setting on one of the City's largest residential lots, this world-class urban estate has been brilliantly re-imagined for the future. A dramatic 40 foot cantilevered infinity pool reflects the shimmering Bay and glittering lights of the City's famed Skyline, while dazzling views of iconic landmarks from Salesforce Tower to The Transamerica Pyramid and beyond provide a stunning panorama surrounding the fabulous resort-like indoor/ outdoor flow of living, dining and entertainment spaces- easily accommodating 300+ guests. The super-advanced, technology enabled health and wellness environment of this sustainably built LEED Platinum Certified, exquisitely designed property, as well as the spa-like guest cottage with steam, sauna, massage room, outdoor shower and hot tub, await the most discerning of owners. Experience the magic of one of California's most extraordinary private residences..."];
+var city = ["San Francisco, CA 94123, Cow Hollow", "San Francisco, CA 94133, Russian Hill", "Houston, TX 77024Piney Point",
+    "Houston, TX 77098", "Brooklyn, NY 11235Sheepshead Bay","Los Angeles, CA 90077","Bel-Air / Holmby Hills, California","Beverly Hills, California","Bel-Air / Holmby Hills, California"];
+
+var bedroom = [4, 6, 5, 3, 4, 6, 17,6,5];
+
+var bathroom = [7, 8, 3, 2, 3, 7, 22,8,6];
+
+var sqrt = [5465, 9495, 3200, 1100,3368, 5777, 30000, 8060 ,8542];
+
+var price = ["$5,000,000", "$6,500,000", "$1,200,000", "$200,000", "$700,000", "$2,450,000", "$8,000,000","$1,250,000","$1,500,000"];
+
+var descriptions = ["Residence 2646 is the pinnacle of San Francisco residential design a vision of what naturally beautiful, blissful functional living should be. It is a sophisticated San Francisco home designed for the contemporary active family with Golden Gate Bridge views, walkability and a seamless indoor-outdoor connection. Overlooking the San Francisco Bay, Residence 2646 celebrates today's contemporary city-dwelling family, streamlining busy lifestyle with innovative technology and stunning design. Ideally located on a desirable residential block, this home delivers convenient walkability to San Francisco's most coveted independent schools and sophisticated shops and restaurants of Union Street. With four en-suite bedrooms on one level. This exceptional LEED Platinum-certified home is rich with extra amenities. Crowning the home is the penthouse level, including space for work and play with both North and South facing terraces and direct access to the roof deck with a fireplace."
+    , "RESIDENCE 950...San Francisco's Newest Masterpiece. Nestled atop a magical park-like setting on one of the City's largest residential lots, this world-class urban estate has been brilliantly re-imagined for the future. A dramatic 40 foot cantilevered infinity pool reflects the shimmering Bay and glittering lights of the City's famed Skyline, while dazzling views of iconic landmarks from Salesforce Tower to The Transamerica Pyramid and beyond provide a stunning panorama surrounding the fabulous resort-like indoor/ outdoor flow of living, dining and entertainment spaces- easily accommodating 300+ guests. The super-advanced, technology enabled health and wellness environment of this sustainably built LEED Platinum Certified, exquisitely designed property, as well as the spa-like guest cottage with steam, sauna, massage room, outdoor shower and hot tub, await the most discerning of owners. Experience the magic of one of California's most extraordinary private residences...",
+"Chateau-inspired residence on over an acre, custom-built for the seller w/ finest artisan finishes, reclaimed materials & repurposed architectural elements. Single-artisan hand-carved millwork, wall finishes by The Artists Hand & ADR-sourced tile accents. Honed travertine & walnut floors; Kane carpet; ceilings w/ rustic French oak beams; granite, marble, onyx & teak surfaces; Conrad window treatments; hand-forged wrought-iron hardware. Two staircases, elevator & fandoliers (ceiling fan/chandelier by Potter Art Metal Studios). Kitchen w/ island w/ teak deck, copper prep sink, Wolf cooktop & Wolf in-counter steamer; cabinet-front SubZero refrigerator w/ drawers; Wolf wall ovens; two dishwashers; copper apron sink; beveled granite countertops. Luxurious first-floor master w/ spa-inspired, onyx & marble bath and bespoke walk-in closets; large en suite bedrooms w/ walk-in closets; playroom; game room; home theater; loggia, summer kitchen, pool w/ baja ledge & spa. Whole house generator.",
+"Beautiful new construction home in Upper Kirby, built by BK Builder; with nearly two decades of custom home construction experience. Features a complete his & hers master suite - 2 Sinks, 2 Toilets, & 2 Closets! All custom woodworking throughout the home - Cabinets/handrails/doors/trim/molding. Private pool with attached hot tub. Full Thermador appliance package - Separate built-in Fridge & Freezer, built-in Microwave & Espresso Machine, 48'' Range w/ Vent Hood. Extra high ceilings, Italian Porcelain tiles, Natural Red Oak flooring - and much, much more!",
+"Remarks: Centrally located fully detached newly rebuilt beauty of almost 3500 sq ft of living space ! The house offers you 4 bedroom 2.5 bathroom and full basement that potentially may be converted to a separate unit. Open concept kitchen with custom made cabinetry , granite counter and stainless steel appliances . All floors are radiant heated throughout the house ( marble and oak parquet ) Second floor consists of 4 bedrooms one of which is a master suite with its own en-suite bathroom! Huge terrace off the master suite. Another full bath with jacuzzi . All bedrooms and foyer offer custom made closets for your convenience . The full house is technologically equipped with surround sound system. Split ductless system air conditioning with multiple thermostats . Basement has plenty of space, light and has all the hookups for any potential use! Garage has heated floors and has an interior entrance connected to the house Beautiful backyard and private driveway! Pack your bags and move in! Brokers, we have the keys. So, all showings are easily arranged. 50/50 commission split!",
+"This Jaw-dropping contemporary new construction in highly desired Bel Air epitomizes Los Angeles living at its finest. Built and designed by ANR Signature Collection, this masterpiece offers it all. From the moment of entry one is greeted with jetliner views from nearly every room of the home, from the San Gabriel mountains, head on downtown Los Angeles, Century City, all the way to the Pacific Ocean. The main floor offers formal living, dining, bedroom suite, a designer kitchen with Miele appliances all overlooking the grassy backyard and expansive views. Upstairs there are four bedrooms all en-suite, including a dreamy Master bed/sitting and master bath with city/ocean views. In addition to the four upstairs bedrooms there is a bonus flex room that could be office, media room, gym, etc. Outside there is an infinity edge pool looking straight onto downtown, covered pool cabana with BBQ and a 3/4th pool bath. This home is in a class of its own.",
+"Fashion designers Lubov and Max Azria's 30,000-square-foot estate on Sunset Boulevard, Maison du Soleil, was built in the 1930s by legendary architect Paul Williams-who helped shape the legacy of classic Hollywood Regency design. The iconic 60-room residence merges colonial architecture with modern design, resulting in a grand estate that's luxurious, chic and timeless. A seamless blend of worldly influences, the property features Japanese and French gardens, a Moroccan-style pool house complete with a custom hammam and floor-to-ceiling marble tile, a spacious 6,000-sq-ft private movie theater with an attached catering kitchen, greenhouse, a glass-walled tennis court, plus multiple dining patios and fire pits. Yet with all of its scale and grandeur, the home remains warm, whimsical and inviting, making it a unique retreat for intimate family life while still poised for large-scale entertaining.",
+"Situated behind gates and elevated high above prime lower San Ysidro Drive in the City of Beverly Hills, this newly constructed residence totaling over 8,000 sf with 3 structures (main house, guest house and pool house) was designed by one of California's greatest living architects, Ray Kappe. A stunning 2 story masterpiece (plus basement) features Kappe's quintessential use of poured in place concrete, glass, teak and redwood that took over 6 years to build and was completed in 2017. Perfect for entertaining, the main house with dramatic open spaces includes a sunken living room, dining room, Bulthaup kitchen, family room and office. The home also features a screening room, gym and wine cellar. There is also a full one bedroom guest house with kitchenette and separate pool house structure. The property which spans approx. 30,000 sf includes an incredible swimmer's pool, large lawns, expansive patios and a viewing deck overlooking the city, canyons and ocean.",
+"Versace Mansion - West. One of a kind Versace-themed estate located in the gated community of Bel Air Crest. This amazing house features recent design and materials upgrades, transforming it into a warm and inviting architectural gem. Enter through the grand front gate and step into the welcoming foyer entry with double staircase adorned by a hanging crystal chandelier, soaring ceilings and majestic windows which flood the main floor with natural sunlight. Enjoy 5 magnificent fireplaces, recessed lighting, custom movie theater, magnificent library with built-in cabinets, and a 4-car garage with epoxy-coated floor and glass roll-up garage door. Extensive use of Gianni Versace-inspired details create an atmosphere of luxurious and warm modernism. Relish the California sunshine as you entertain guests in the expansive grassy yard, complete with pool, spa, lounge, fire pit, BBQ, and outdoor kitchen."];
 var temp;
 
 window.onload = function()
