e65508084f05dc6139cd0c91276b28bb83bf8aa8
BeharRexhepi <39538259+BeharRexhepi@users.noreply.github.com>
2020-01-02 20:56:03 +0100

    Completed the filter on 'Buy' page

--- js/FilterData.js
+++ js/FilterData.js
@@ -1,4 +1,4 @@
-  var changes = 0;         
+        
 function filterData()
 {
     
@@ -32,20 +32,48 @@ function filterData()
 
     var imgInfo = document.getElementsByClassName("imgInfo");
     
-    var element = imgBox[13];
-    
-//    if(changes===0)
-//    {
-//        document.getElementsByClassName("photos")[0].appendChild(element);
-//        changes++;
-//    }
-    
-
     var count = 0;
+    
+    for(var i=0; i<imgBox.length; i++)
+    {
+        var position = imgBox[i].getElementsByClassName("position")[0].textContent;
+        position=parseInt(position);
+        if(position<12)
+        {
+             //document.getElementsByClassName("photos")[0].appendChild(imgBox[i]);
+           var item = imgBox[i];
+           var list = document.getElementsByClassName("photos")[0];
+           list.insertBefore(item, list.childNodes[position]);
+             
+        }
+        else
+        {
+           document.getElementsByClassName("photos")[1].appendChild(imgBox[i]);
+        }
+    }
+    
+    
+    //sortimi i faqes 2
+    for(var i=12; i<24; i++)
+    {
+        
+        for(var j=12; j<24; j++)
+        {
+            var position = imgBox[j].getElementsByClassName("position")[0].textContent;
+            position=parseInt(position);
+            if(position===i)
+            {
+                var item = imgBox[j];
+                var list = document.getElementsByClassName("photos")[1];
+                list.insertBefore(item, list.childNodes[position]);
+            }  
+            
+        }
+    }
+
       
     for(var i=0; i<imgBox.length; i++)
     {
-        
         
         var price = imgInfo[i].getElementsByClassName("price")[0].textContent;
         price = Number(price.replace(/[^0-9.-]+/g,""));
@@ -59,28 +87,28 @@ function filterData()
         sqrfe = Number(sqrfe.replace(/[^0-9.-]+/g,""));
         
         
-//        if((price<minPrice || (price>maxPrice && maxPrice !==0)))
-//        {
-//            imgBox[i].style.display="none";
-//        }
-         if(filterByCity!==city && filterByCity !== "City")
+        if((price<minPrice || (price>maxPrice && maxPrice !==0)))
         {
             imgBox[i].style.display="none";
         }
-//        else if((sqrfe<minSqrfe || (sqrfe>maxSqrfe && maxSqrfe !== 0)))
-//        {
-//            imgBox[i].style.display="none";
-//        }
-//        else if(bedroom<numOfBedrooms)
-//        {
-//            imgBox[i].style.display="none";
-//        }
+        else if(filterByCity!==city && filterByCity !== "City")
+        {
+            imgBox[i].style.display="none";
+        }
+        else if((sqrfe<minSqrfe || (sqrfe>maxSqrfe && maxSqrfe !== 0)))
+        {
+            imgBox[i].style.display="none";
+        }
+        else if(bedroom<numOfBedrooms)
+        {
+            imgBox[i].style.display="none";
+        }
        
         else
         {
             imgBox[i].style.display="";
             count++;
-           
+            
             if(count%3===0)
             {
                 imgBox[i].className="imgBox lastBox";
@@ -94,8 +122,6 @@ function filterData()
              if(i>11 && count<=12)
             {
                document.getElementsByClassName("photos")[0].appendChild(imgBox[i]);
-               //document.getElementsByClassName("photos")[1].style.display="none";
-               
             }
             
 
@@ -103,47 +129,41 @@ function filterData()
         
     }
     
-//              var firstDivLength = document.getElementsByClassName("photos")[0].childElementCount;
-//              
-//              var x=0;
-//              for(var i=0; i<firstDivLength; i++)
-//              {
-//                  if(imgBox[i].style.display!=="none")
-//                  {
-//                      x++;
-//                  }
-//                 
-//              }
-//              
-//              
-//              
-//             
-//
-//              for(var j=x-1; j>=12; j--)
-//                 {
-//                     document.getElementsByClassName("photos")[1].appendChild(imgBox[j]);
-//                 }
-//                 
-//               firstDivLength = document.getElementsByClassName("photos")[0].childElementCount;
-//               
-//              
-//               var secondDivLength=document.getElementsByClassName("photos")[1].childElementCount;
-//               
-//               
-//               var y =0;
-//               for(var i=firstDivLength; i< firstDivLength + secondDivLength; i++)
-//               {
-//                   if(imgBox[i].style.display!=="none")
-//                  {
-//                      y++;
-//                      if(y%3===0)imgBox[i].className="imgBox lastBox";
-//                      else imgBox[i].className="imgBox";
-//                  }
-//               }
-               
-               console.log(count);
-               count=0;
-             }
-   
+    //nese ka ma pak se 12 shpallje qe i plotsojn kriteret nga filtrimi, me u hjek opsioni me navigu ne faqen e dyte
+    if(count<=12)
+    {
+        document.getElementById("page2").style.display="none";
+        changePage(1);
+    }
+    else
+    {
+        document.getElementById("page2").style.display="";
+    }
+    
+    //nese asnje shpallje nuk e plotson kriterin, me e shfaq canvasin dhe me i hjek pjest e navigimit
+    if(count===0)
+    {
+        document.getElementById("notFound").style.display="";
+         document.getElementById("page1").style.display="none";
+    }
+    else
+    {
+        document.getElementById("notFound").style.display="none";
+        document.getElementById("page1").style.display="";
+    }
+    
+    var check = document.getElementsByClassName("photos")[1].style.display;
+    if(count<=2 || (check!=="none"&&count>12&&count<=14))
+    {
+        document.getElementById("navigationWrapper").style="position:absolute; bottom:-30px;";
+    }
+    else
+    {
+        document.getElementById("navigationWrapper").style="";
+    }
+    
+
+} 
 
             
+
